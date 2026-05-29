f721143514f97d5ed47a949d461b08e6c4656311
sLxvly <heromolina4@gmail.com>
2026-05-29 07:57:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("System Online. Connection established.");
+    // You can add logic here to customize the chat experience later
+});
